5708815089c2a890762286ae89c1ea7a720d1fe1
DarkCoder! <87529378+Ankitthalwal@users.noreply.github.com>
2024-07-30 07:59:29 +0530

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -2,8 +2,6 @@ using Excel = Microsoft.Office.Interop.Excel;
 using System;
 using System.Data;
 using System.Data.SqlClient;
-using System.Data.SQLite;
-using System.Reflection.Emit;
 using System.Windows.Forms;
 
 namespace Exceldata
@@ -36,8 +34,8 @@ namespace Exceldata
                 }
             }
 
-            // Display data from SQLite database
-            DisplayDataFromSQLite();
+            // Display data from SQL Server database
+            DisplayDataFromSQLServer();
         }
 
         private void ReadAndInsertExcelData(string filePath)
@@ -50,12 +48,15 @@ namespace Exceldata
 
             try
             {
-                using (SQLiteConnection conn = new SQLiteConnection("Data Source=E:\\database\\sms.db;Version=3;"))
+                using (SqlConnection conn = new SqlConnection("Data Source=VENOM\\SQLEXPRESS;Initial Catalog=Studentdb;Integrated Security=True;Encrypt=False"))
                 {
                     conn.Open();
 
-                    // Create table based on Excel columns
-                    string createTableQuery = $"CREATE TABLE IF NOT EXISTS [{tableName}] (";
+                    // Check if the table exists, if not create it
+                    string checkTableQuery = $"IF OBJECT_ID(N'{tableName}', 'U') IS NULL " +
+                                             "BEGIN " +
+                                             $"CREATE TABLE [{tableName}] (";
+
                     for (int col = 1; col <= range.Columns.Count; col++)
                     {
                         string columnName = (range.Cells[1, col] as Excel.Range).Value2.ToString().Replace(" ", "_");
@@ -63,21 +64,21 @@ namespace Exceldata
                         if (col == 1)
                         {
                             // Assuming the first column is the primary key and it is an integer
-                            createTableQuery += $"{columnName} INTEGER PRIMARY KEY";
+                            checkTableQuery += $"{columnName} INTEGER PRIMARY KEY";
                         }
                         else
                         {
-                            createTableQuery += $", {columnName} TEXT";
+                            checkTableQuery += $", {columnName} TEXT";
                         }
                     }
-                    createTableQuery += ")";
+                    checkTableQuery += ") END";
 
-                    using (SQLiteCommand createTableCmd = new SQLiteCommand(createTableQuery, conn))
+                    using (SqlCommand createTableCmd = new SqlCommand(checkTableQuery, conn))
                     {
                         createTableCmd.ExecuteNonQuery();
                     }
 
-                    using (SQLiteTransaction transaction = conn.BeginTransaction())
+                    using (SqlTransaction transaction = conn.BeginTransaction())
                     {
                         for (int row = 2; row <= range.Rows.Count; row++)
                         {
@@ -92,7 +93,7 @@ namespace Exceldata
                             }
                             insertQuery += ")";
 
-                            using (SQLiteCommand insertCmd = new SQLiteCommand(insertQuery, conn))
+                            using (SqlCommand insertCmd = new SqlCommand(insertQuery, conn, transaction))
                             {
                                 for (int col = 1; col <= range.Columns.Count; col++)
                                 {
@@ -131,23 +132,25 @@ namespace Exceldata
             }
         }
 
-        private void DisplayDataFromSQLite()
+        private void DisplayDataFromSQLServer()
         {
-            using (SQLiteConnection conn = new SQLiteConnection("Data Source=E:\\database\\sms.db;Version=3;"))
+            using (SqlConnection conn = new SqlConnection("Data Source=VENOM\\SQLEXPRESS;Initial Catalog=Studentdb;Integrated Security=True;Encrypt=False"))
             {
                 conn.Open();
 
-                // You may need to adjust this query to dynamically select the table
-                using (SQLiteCommand cmd = new SQLiteCommand("SELECT name FROM sqlite_master WHERE type='table';", conn))
+                // Query to get the first user table
+                string getTablesQuery = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'";
+
+                using (SqlCommand cmd = new SqlCommand(getTablesQuery, conn))
                 {
-                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    using (SqlDataReader reader = cmd.ExecuteReader())
                     {
                         if (reader.Read())
                         {
                             string tableName = reader.GetString(0);
-                            using (SQLiteCommand selectCmd = new SQLiteCommand($"SELECT * FROM [{tableName}]", conn))
+                            using (SqlCommand selectCmd = new SqlCommand($"SELECT * FROM [{tableName}]", conn))
                             {
-                                using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(selectCmd))
+                                using (SqlDataAdapter adapter = new SqlDataAdapter(selectCmd))
                                 {
                                     System.Data.DataTable dataTable = new System.Data.DataTable();
                                     adapter.Fill(dataTable);
@@ -159,22 +162,21 @@ namespace Exceldata
                 }
             }
         }
-
-
-        //update the data of in the current database 
-        private void btn2(object sender,EventArgs e)
+        private void btn2(object sender, EventArgs e)
         {
 
         }
+
         private void btn2_Click(object sender, EventArgs e)
         {
-            
-            using (SQLiteConnection conn = new SQLiteConnection("Data Source=E:\\database\\sms.db;Version=3;"))
+            using (SqlConnection conn = new SqlConnection("Data Source=VENOM\\SQLEXPRESS;Initial Catalog=Studentdb;Integrated Security=True;Encrypt=False"))
             {
                 conn.Open();
 
                 // Create table based on DataGridView columns
-                string createTableQuery = "CREATE TABLE IF NOT EXISTS dummy (";
+                string createTableQuery = "IF OBJECT_ID(N'dummy', 'U') IS NULL " +
+                                          "BEGIN " +
+                                          "CREATE TABLE dummy (";
                 for (int i = 0; i < dataGridView1.Columns.Count; i++)
                 {
                     string columnName = dataGridView1.Columns[i].HeaderText.Replace(" ", "_");
@@ -186,28 +188,22 @@ namespace Exceldata
                         createTableQuery += ", ";
                     }
                 }
-                createTableQuery += ");";
+                createTableQuery += ") END";
 
-
-
-            
-
-                using (SQLiteCommand createTableCmd = new SQLiteCommand(createTableQuery, conn))
+                using (SqlCommand createTableCmd = new SqlCommand(createTableQuery, conn))
                 {
                     createTableCmd.ExecuteNonQuery();
                 }
 
-                using (SQLiteTransaction transaction = conn.BeginTransaction())
+                using (SqlTransaction transaction = conn.BeginTransaction())
                 {
-                    using (SQLiteCommand comm = new SQLiteCommand())
+                    using (SqlCommand comm = new SqlCommand())
                     {
                         comm.Connection = conn;
                         comm.Transaction = transaction;
 
                         try
                         {
-                            
-
                             for (int row = 0; row < dataGridView1.Rows.Count - 1; row++) // Exclude the new row placeholder
                             {
                                 string insertQuery = "INSERT INTO dummy VALUES (";
@@ -221,7 +217,7 @@ namespace Exceldata
                                 }
                                 insertQuery += ")";
 
-                                using (SQLiteCommand insertCmd = new SQLiteCommand(insertQuery, conn))
+                                using (SqlCommand insertCmd = new SqlCommand(insertQuery, conn))
                                 {
                                     for (int col = 0; col < dataGridView1.Columns.Count; col++)
                                     {
