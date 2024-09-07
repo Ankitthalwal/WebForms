be857d90688d11c9f3c7a4c21fc2e7c1d789bc66
DarkCoder! <87529378+Ankitthalwal@users.noreply.github.com>
2024-09-07 07:54:39 +0530

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,227 +1,110 @@
 using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Data.SQLite;
-using System.Data;
 using System.IO;
-using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using ExcelDataReader;
-namespace Cameradetailstodb
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Text.RegularExpressions;
+
+namespace checks
 {
     public partial class Form1 : Form
     {
-        string connectionString;
-        string tableName = "T01_tempdata";
-        private string excelFilePath;
-        private DataTable ExcelData = new DataTable();
-        List<string> Table_headers1 = new List<string>();
-
-        string directoryPath = @"E:\CATRAT\database";
-        string databaseName = "MyDatabase.sqlite";
-        SQLiteConnection conn;
+        public string selectedPath;
 
         public Form1()
         {
             InitializeComponent();
-            string databasePath = Path.Combine(directoryPath, databaseName);
-            connectionString = $"Data Source={databasePath};Version=3;";
-            conn = new SQLiteConnection(connectionString);
         }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            using(conn)
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
             {
-                conn.Open();
-                create_database();
-                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                if (dialog.ShowDialog() == DialogResult.OK)
                 {
-                    openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
-                    if (openFileDialog.ShowDialog() == DialogResult.OK)
-                    {
-                        excelFilePath = openFileDialog.FileName;
-                        if (Validate_ExcelSheet_with_dbTable())
-                        {
-                            ReadAndStoreExcelData();
-                            dataGridView1.DataSource = ExcelData;
-                           
-                        }
-                        else
-                        {
-                            MessageBox.Show("Excel Sheet Columns do not match with our table columns");
-                        }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Please select an Excel File.");
-                    }
+                    selectedPath = dialog.SelectedPath;
+                    PerformChecks();
                 }
-
-                conn.Close();
             }
-            
         }
 
-        private void create_database()
+        private async void PerformChecks()
         {
-            
-            //using (conn)
-            //{
-            //    conn.Open();
-                string sql = $"CREATE TABLE IF NOT EXISTS [{tableName}] (" +
-                             "ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                             "Camera_ID TEXT, Block_ID INTEGER NOT NULL, Long_D REAL NOT NULL, " +
-                             "Long_M REAL NOT NULL, Long_S REAL NOT NULL, Lat_D REAL NOT NULL, lat_M REAL NOT NULL, " +
-                             "Lat_S REAL NOT NULL, Remarks TEXT, D1 TEXT NOT NULL, D2 TEXT NOT NULL, D3 TEXT NOT NULL, " +
-                             "D4 TEXT NOT NULL, D5 TEXT NOT NULL)";
-                using (SQLiteCommand command = new SQLiteCommand(sql, conn))
+            string[] directories = selectedPath.Split(Path.DirectorySeparatorChar);
+
+            for (int i = 1; i < directories.Length; i++)
+            {
+                string dir = directories[i];
+
+                // Replace spaces with underscores
+                if (dir.Contains(" "))
                 {
-                    command.ExecuteNonQuery();
+                    directories[i] = dir.Replace(" ", "_");
+                    MessageBox.Show($"Renamed '{dir}' to '{directories[i]}' due to spaces.");
                 }
-                //conn.Close();
-           // }
+
+                // Check if the directory starts or ends with an underscore
+                if (dir.StartsWith("_") || dir.EndsWith("_"))
+                {
+                    MessageBox.Show($"Directory '{dir}' starts or ends with an underscore.");
+                    return;
+                }
+
+                // Check if the directory starts or ends with a digit
+                if (char.IsDigit(dir[0]) || char.IsDigit(dir[dir.Length-1])) 
+                {
+                    MessageBox.Show($"Directory '{dir}' starts or ends with a digit.");
+                    return;
+                }
+
+                // Check if the directory contains special characters
+                if (ContainsSpecialCharacter(dir))
+                {
+                    MessageBox.Show($"Directory '{dir}' contains special characters.");
+                    checkBox3.Visible = true;
+                    return;
+                }
+            }
+
+            await UpdateUI();
+            selectedPath = string.Join(Path.DirectorySeparatorChar.ToString(), directories);
         }
 
-        private void ReadAndStoreExcelData()
+        private bool ContainsSpecialCharacter(string directory)
         {
-            using (var stream = File.Open(excelFilePath, FileMode.Open, FileAccess.Read))
-            {
-                using (var reader = ExcelReaderFactory.CreateReader(stream))
-                {
-                    var configuration1 = new ExcelDataSetConfiguration
-                    {
-                        ConfigureDataTable = _ => new ExcelDataTableConfiguration
-                        {
-                            UseHeaderRow = true 
-                        }
-                    };
-
-                    var dataSet = reader.AsDataSet(configuration1);
-
-                    if (dataSet.Tables.Count > 0)
-                    {
-                        var sheet = dataSet.Tables[0]; 
-
-                        // Add columns to the filtered table only if they are in Table_headers1
-                        foreach (DataColumn column in sheet.Columns)
-                        {
-                            var headerString = column.ColumnName;
-                            if (Table_headers1.Contains(headerString.ToLower()))
-                            {
-                               ExcelData.Columns.Add(headerString);
-                            }
-                        }
-                        // Add rows to the filtered table
-
-                        
-                        using (var transaction = conn.BeginTransaction())
-                        {                           
-                            foreach (DataRow row in sheet.Rows)
-                            {
-                                var newRow = ExcelData.NewRow();
-                                foreach (DataColumn column in ExcelData.Columns)
-                                {
-                                    var headerString = column.ColumnName;
-                                    var originalColumnIndex = sheet.Columns.IndexOf(headerString);
-                                    if (originalColumnIndex >= 0)
-                                    {
-                                        newRow[headerString] = row[originalColumnIndex];
-                                    }
-                                }                             
-                                ExcelData.Rows.Add(newRow);
-
-                                string query = $@"INSERT INTO [{tableName}] (Camera_ID, Block_ID, Long_D, Long_M, Long_S, Lat_D, Lat_M, Lat_S, Remarks, D1, D2, D3, D4, D5) 
-                                VALUES (@Camera_ID, @Block_ID, @Long_D, @Long_M, @Long_S, @Lat_D, @Lat_M, @Lat_S, @Remarks, @D1, @D2, @D3, @D4, @D5)";
-
-                                using (SQLiteCommand comm = new SQLiteCommand(query, conn))
-                                {
-                                    comm.Parameters.AddWithValue("@Camera_ID",newRow["Camera_ID"] ?? DBNull.Value);
-                                    comm.Parameters.AddWithValue("@Block_ID", newRow["Block_ID"] ?? DBNull.Value);
-                                    comm.Parameters.AddWithValue("@Long_D", newRow["Long_D"] ?? DBNull.Value);
-                                    comm.Parameters.AddWithValue("@Long_M", newRow["Long_M"] ?? DBNull.Value);
-                                    comm.Parameters.AddWithValue("@Long_S", newRow["Long_S"] ?? DBNull.Value);
-                                    comm.Parameters.AddWithValue("@Lat_D", newRow["Lat_D"] ?? DBNull.Value);
-                                    comm.Parameters.AddWithValue("@Lat_M", newRow["Lat_M"] ?? DBNull.Value);
-                                    comm.Parameters.AddWithValue("@Lat_S", newRow["Lat_S"] ?? DBNull.Value);
-                                    comm.Parameters.AddWithValue("@Remarks", newRow["Remarks"] ?? DBNull.Value);
-                                    comm.Parameters.AddWithValue("@D1", newRow["D1"] ?? DBNull.Value);
-                                    comm.Parameters.AddWithValue("@D2", newRow["D2"] ?? DBNull.Value);
-                                    comm.Parameters.AddWithValue("@D3", newRow["D3"] ?? DBNull.Value);
-                                    comm.Parameters.AddWithValue("@D4", newRow["D4"] ?? DBNull.Value);
-                                    comm.Parameters.AddWithValue("@D5", newRow["D5"] ?? DBNull.Value);
-                                    comm.ExecuteNonQuery();                                   
-                                }                                
-                            }
-                            transaction.Commit();
-                        }
-                    }
-                    reader.Close();
-                }
-                stream.Close();
-            }
+            return Regex.IsMatch(directory, @"[^a-zA-Z0-9_ ]");
         }
-        private bool Validate_ExcelSheet_with_dbTable()
+
+
+        private async Task UpdateUI()
         {
-            List<string> Excel_headers = new List<string>();
-            //add excel headers into the  Excel_headers list
+            // Simulate UI update delays
+            await Task.Delay(500);
+            checkBox1.Visible = true;
+            checkBox1.Text = " Selected Folder Blanked Checked!";
+            checkBox1.Checked = true;
 
-            using (var stream = File.Open(excelFilePath, FileMode.Open, FileAccess.Read))
-            {
-                using (var reader = ExcelReaderFactory.CreateReader(stream))
-                {
-                    var configuration1 = new ExcelDataSetConfiguration
-                    {
-                        ConfigureDataTable = _ => new ExcelDataTableConfiguration
-                        {
-                            UseHeaderRow = true 
-                        }
-                    };
+            await Task.Delay(500);
+            checkBox2.Visible = true;
+            checkBox2.Text = " Selected Folder Digit checked!";
+            checkBox2.Checked = true;
 
-                    var dataSet = reader.AsDataSet(configuration1);
+            await Task.Delay(500);
+            checkBox3.Visible = true;
+            checkBox3.Text = "Selected Folder Underscore Checked!";
+            checkBox3.Checked = true;
 
-                    if (dataSet.Tables.Count > 0)
-                    {
-                        var sheet = dataSet.Tables[0]; //Take the first sheet
+            await Task.Delay(500);
+            checkBox4.Visible = true;
+            checkBox4.Text = "Selected Path Length checked!";
+            checkBox4.Checked = true;
 
-                    
-               
-                        // Add columns to the filtered table only if they are in Table_headers1
-                        foreach (DataColumn column in sheet.Columns)
-                        {
-                            var headerString = column.ColumnName.ToString().ToLower();
-                            Excel_headers.Add(headerString);
-                        }
-                    }
-                }
-            }
-
-
-              // Store all headers of database table to list (Table_headers1)
-             
-                string sql = $"PRAGMA table_info('{tableName}');";
-                using (SQLiteCommand command = new SQLiteCommand(sql, conn))
-                {
-                    using (SQLiteDataReader reader = command.ExecuteReader())
-                    {
-                        Table_headers1.Clear();
-                        Table_headers1.AddRange(reader.Cast<DbDataRecord>()
-                            .Select(record => record["name"].ToString().ToLower()));
-                    }
-               
-            }
-
-            // Compare that Excel sheet has the same headers as the table has otherwise do not accept Excel sheet
-            bool allHeadersContained = Table_headers1.All(h1 => Excel_headers.Contains(h1));
-            if (allHeadersContained)
-            {
-                MessageBox.Show("Excel headers match database table columns.");
-                return true;
-            }
-            else
-            {
-                MessageBox.Show("Excel headers do not match database table columns.");
-                return false;
-            }
+            await Task.Delay(500);
+            checkBox5.Visible = true;
+            checkBox5.Text = "Selected Folder White Spaces checked!";
+            checkBox5.Checked = true;
+            textBox1.Text = selectedPath;
         }
     }
 }
